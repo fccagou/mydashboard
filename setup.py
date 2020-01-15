380001937df69be5934b4b3c7aaf9a9f02a138dc
fccagou <fccagou@gmail.com>
2020-01-15 20:01:22 +0100

    doc(copyright): update date and mail

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@
 # -*- encoding: utf-8 -*-
 
 # This file is part of mydashboard.
-# Copyright 2019 fccagou <fccagou@gmail.com>
+# Copyright 2019-2020 fccagou <me@fccagou.fr>
 #
-# PYAP is free software: you can redistribute it and/or modify it
+# mydashboard is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.

cc0b77859aa86f2b8b0845d8fda22f6123fda055
Guillaume COURS <guillaume.cours@yahoo.fr>
2022-02-06 23:52:55 +0100

    refact(NavBar): use router-link instead a

--- docroot/js/components/NavBar.vue.js
+++ docroot/js/components/NavBar.vue.js
@@ -2,7 +2,7 @@ const NavBarComponent = {
   template: `
     <nav class="navbar navbar-expand-lg navbar-dark bg-success">
       <div class="container-fluid">
-        <a class="navbar-brand" href="/">My Dashboard</a>
+        <router-link class="navbar-brand" :to="{ name: 'RemoteAccess' }">My Dashboard</router-link>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>

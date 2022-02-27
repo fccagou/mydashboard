3c915016b454c521128f328305303917ed1d9c40
Guillaume COURS <guillaume.cours@yahoo.fr>
2022-02-27 22:47:21 +0100

    feat(NotFound): add 404 ascii art

--- docroot/js/views/NotFound.vue.js
+++ docroot/js/views/NotFound.vue.js
@@ -1,3 +1,14 @@
 const NotFoundComponent = {
-    template: `404 page!`,
+    template: `<div class="h-100 d-flex flex-column align-items-center justify-content-center">
+    <div class="ascii-art">
+    &nbsp;___&nbsp;&nbsp;&nbsp;___&nbsp;&nbsp;&nbsp;________&nbsp;&nbsp;&nbsp;___&nbsp;&nbsp;&nbsp;___<br>
+    |\\&nbsp;&nbsp;\\&nbsp;|\\&nbsp;&nbsp;\\&nbsp;|\\&nbsp;&nbsp;&nbsp;__&nbsp;&nbsp;\\&nbsp;|\\&nbsp;&nbsp;\\&nbsp;|\\&nbsp;&nbsp;\\<br>
+    \\&nbsp;\\&nbsp;&nbsp;\\\\_\\&nbsp;&nbsp;\\\\&nbsp;\\&nbsp;&nbsp;\\|\\&nbsp;&nbsp;\\\\&nbsp;\\&nbsp;&nbsp;\\\\_\\&nbsp;&nbsp;\\<br>
+    &nbsp;\\&nbsp;\\______&nbsp;&nbsp;\\\\&nbsp;\\&nbsp;&nbsp;\\\\\\&nbsp;&nbsp;\\\\&nbsp;\\______&nbsp;&nbsp;\\<br>
+    &nbsp;&nbsp;\\|_____|\\&nbsp;&nbsp;\\\\&nbsp;\\&nbsp;&nbsp;\\\\\\&nbsp;&nbsp;\\\\|_____|\\&nbsp;&nbsp;\\<br>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\\&nbsp;\\__\\\\&nbsp;\\_______\\&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\\&nbsp;\\__\\<br>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\\|__|&nbsp;\\|_______|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\\|__|<br>
+    </div>
+    <div class="ascii-art">NOT FOUND</div>
+</div>`,
 };

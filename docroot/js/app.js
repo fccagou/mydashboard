28b5e12a8c28a7f014ca6d84aa2f2236eed0843d
Guillaume COURS <guillaume.cours@yahoo.fr>
2022-02-27 22:23:46 +0100

    feat(css): fill page in height

--- docroot/js/app.js
+++ docroot/js/app.js
@@ -2,7 +2,7 @@ const app = Vue.createApp({
     el: '#app',
     template: `
     <nav-bar/>
-    <div class="container-fluid px-4">
+    <div class="container-fluid px-4 flex-grow-1">
         <router-view class="mt-4"/>
     </div>
     <toasts/>`,

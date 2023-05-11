491442ce65a5adba0b8d41bee6b61f7f56d08aff
Guillaume COURS <guillaume.cours@yahoo.fr>
2023-05-11 23:32:33 +0200

    doc(api): add spec with swagger

---
+++ doc/launch_swagger.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+command="run --rm -p 8000:8080 -u$(id -u):$(id -g) -e SWAGGER_JSON=/spec_api.yaml -v ${SCRIPT_DIR}/spec_api.yaml:/spec_api.yaml swaggerapi/swagger-ui"
+
+# check if docker is present on the system otherwise use podman instead
+if command -v docker > /dev/null
+then
+    /usr/bin/docker $command
+elif command -v podman > /dev/null
+then
+    /usr/bin/podman $command
+else
+    echo "Neither docker nor podman are installed on the system"
+    exit 1
+fi

4ebe59701fb8f0cc11891b632a7342c6e9baeeef
Guillaume COURS <guillaume.cours@yahoo.fr>
2023-05-13 19:18:23 +0200

    feat(NavBar): use ul>li for items

--- docroot/js/components/NavBar.vue.js
+++ docroot/js/components/NavBar.vue.js
@@ -3,14 +3,16 @@ const NavBarComponent = {
     <nav class="navbar navbar-expand-lg navbar-dark bg-success shadow">
       <div class="container-fluid">
         <router-link class="navbar-brand fs-4 fw-bold" :to="{ name: 'RemoteAccess' }">My Dashboard</router-link>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
           <i class="bi bi-list"></i>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <div class="navbar-nav me-auto">
-            <a class="nav-link" target="_blank" v-for="(link, index) in links" :key="index" :href="link.url">{{ link.label }}</a>
-          </div>
+        <div class="collapse navbar-collapse" id="navbar">
+          <ul class="navbar-nav me-auto">
+            <li class="nav-item" v-for="(link, index) in links" :key="index">
+              <a class="nav-link" target="_blank" :href="link.url">{{ link.label }}</a>
+            </li>
+          </ul>
 
           <router-link type="button" class="btn btn-success btn-settings rounded-pill" :to="{ name: 'UserPreferences' }"><i class="bi bi-gear-fill"></i> Settings</router-link>
         </div>

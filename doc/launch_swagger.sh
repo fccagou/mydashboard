7a30a9b37113a796323a4e79da1834ea4d079461
fccagou <me@fccagou.fr>
2023-05-14 17:49:45 +0200

    feat(swagger): add name and daemon mode to swagger launcher

--- doc/launch_swagger.sh
+++ doc/launch_swagger.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-command="run --rm -p 8000:8080 -u$(id -u):$(id -g) -e SWAGGER_JSON=/spec_api.yaml -v ${SCRIPT_DIR}/spec_api.yaml:/spec_api.yaml swaggerapi/swagger-ui"
+command="run --rm -d --name mydashboard_swagger -p 8000:8080 -u$(id -u):$(id -g) -e SWAGGER_JSON=/spec_api.yaml -v ${SCRIPT_DIR}/spec_api.yaml:/spec_api.yaml swaggerapi/swagger-ui"
 
 # check if docker is present on the system otherwise use podman instead
 if command -v docker > /dev/null

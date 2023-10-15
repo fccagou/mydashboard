6667fd9547753b837e7c09a500d6a1e2981bbcf2
Guill4aume001 <guillaume@cours.cloud>
2023-10-15 22:02:24 +0200

    feat(PreferenceInput): add label for resolution type

--- docroot/js/components/PreferenceInput.vue.js
+++ docroot/js/components/PreferenceInput.vue.js
@@ -6,10 +6,13 @@ const PreferenceInputComponent = {
       <input class="form-check-input" type="checkbox" id="switch-{{ name }}" :checked="value" @input="updateBoolean">
       <label class="form-check-label" for="switch-{{ name }}">{{ formattedName }}</label>
       </div>
-    <div v-else-if="type === 'resolution'" class="input-group mb-3">
-      <input type="number" min="0" class="form-control" :placeholder="this.default.width" aria-label="Width" v-model="value.width" @input="updateResolution">
-      <span class="input-group-text">x</span>
-      <input type="number" min="0" class="form-control" :placeholder="this.default.height" aria-label="Heigh" v-model="value.height" @input="updateResolution">
+    <div v-else-if="type === 'resolution'" class="mb-3">
+      <label class="form-label">{{ formattedName }}</label>
+      <div class="input-group">
+        <input type="number" min="0" class="form-control" :placeholder="this.default.width" aria-label="Width" v-model="value.width" @input="updateResolution">
+        <span class="input-group-text">x</span>
+        <input type="number" min="0" class="form-control" :placeholder="this.default.height" aria-label="Heigh" v-model="value.height" @input="updateResolution">
+      </div>
     </div>
     <div v-else class="form-group">
         <label for="input-{{ name }}">{{ formattedName }}</label>
